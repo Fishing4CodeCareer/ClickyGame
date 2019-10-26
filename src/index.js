b83a4d8b8a0d3498dee571bbf8674c45ea9585d1
Allan Pearson <Alprsn2012@gmail.com>
2019-10-25 19:12:08 -0500

    Call the App rendered from the Dom by the root

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./App";
+import "./index.css";
+
+ReactDom.render(<App />, document.getElementById("root"));
+
+///Call the App rendered from the Dom by the root///
