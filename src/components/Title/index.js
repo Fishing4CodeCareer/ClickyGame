e04774957210da0b3593d08f2370834de928b774
Allan Pearson <Alprsn2012@gmail.com>
2019-10-25 19:52:21 -0500

    Call the data from JSX.element in ./styles.css to return///// ////title to return a string to the props.children functionality

---
+++ src/components/Title/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Title(props) {
+    return <h1 className="title">{props.children}</h1>;
+    
+}
+
+export default Title;
+
+/// Call the data from JSX.element in ./styles.css to return/////
+////title to return a string to the props.children functionality//  
